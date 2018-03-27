1729496c8088d5054bd02b4d28e8a52c99ad2490
rr- <rr-@sakuya.pl>
2018-03-27 10:36:31 +0200

    Add autocomplete to the example

--- example/example.py
+++ example/example.py
@@ -1,13 +1,25 @@
 import urwid
 import urwid_readline
 
+
 def show_or_exit(key):
     if key in ('ctrl q', 'ctrl Q'):
         raise urwid.ExitMainLoop()
     txt.set_edit_text('unknown key: ' + repr(key))
     txt.set_edit_pos(len(txt.edit_text))
 
+
+def compl(text, state):
+    cmd = ('start', 'stop', 'next')
+    tmp = [c for c in cmd if c and c.startswith(text)] if text else cmd
+    try:
+        return tmp[state]
+    except IndexError:
+        return None
+
+
 txt = urwid_readline.ReadlineEdit()
+txt.enable_autocomplete(compl)
 fill = urwid.Filler(txt, 'top')
 loop = urwid.MainLoop(fill, unhandled_input=show_or_exit)
 loop.run()
