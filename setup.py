af95e678353e2171e8e3ec5a9ed281cec27e92a2
rr- <rr-@sakuya.pl>
2018-03-28 21:04:03 +0200

    Release 0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='urwid_readline',
     description=(
         'A textbox edit widget for urwid that supports readline shortcuts'),
-    version='0.4',
+    version='0.5',
     url='https://github.com/rr-/urwid_readline',
     packages=find_packages(),
 
