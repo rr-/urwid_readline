38c46143d9b9be1b97544d91b7f79ac0e216fc26
Marcin Kurczewski <dash@wind.garden>
2024-09-22 19:45:08 +0200

    Release 0.15.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description=(
         "A textbox edit widget for urwid that supports readline shortcuts"
     ),
-    version="0.14",
+    version="0.15.1",
     url="https://github.com/rr-/urwid_readline",
     packages=find_packages(),
     install_requires=["urwid"],
